0440a605a62c70cd3ae13cf98911883bd4d64015
Jason MacWilliams <jason@discoverygarden.ca>
2011-11-21 10:23:21 -0400

    master ingest script to handle things like: required root access, python command line

--- Ingest.sh
+++ Ingest.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-python Ingester.py > >(tee stdout.log) 2> >(tee stderr.log >&2)
+# script is running as root
+if [ $EUID -ne 0 ]; then
+    echo "This script requires root access to run"
+else
+    python Ingester.py > >(tee stdout.log) 2>stderr.log
+fi
