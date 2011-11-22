5e14fcb81ae118cad5b8b6d05c64d0a6179fcc12
Jason MacWilliams <jason@discoverygarden.ca>
2011-11-22 16:25:25 -0400

    Added email object to collect a cumulative email message and then send it all at once for a more detailed report

--- Mailer.py
+++ Mailer.py
@@ -2,9 +2,6 @@
 Created on Oct. 12 2011
 
 @author: Jason MacWilliams
-@dependencies: lxml
-
-@TODO: email integration:waiting on server
 """
 
 import subprocess
@@ -18,3 +15,35 @@ def sendEmail(addrs, subject, message):
     # XXX: we might want to attach the logfile or something else here.  In that case the order of
     # the print statement and the sendmail should be reversed so the print statement doesn't appear
     # in the log
+
+class EmailMessage:
+    def __init__(self, subject="", addrs=[]):
+        if type(addrs) != list:
+            return
+        self.subject = subject
+        self.addrs = addrs
+        self.message = ""
+
+    def addAddress(self, addr):
+        if type(addr) == str and not addr in self.addrs:
+            self.addrs.append(addr)
+
+    def removeAddress(self, addr):
+        if type(addr) == str and addr in self.addrs:
+            self.addrs.remove(addr)
+
+    def setSubject(self, subject):
+        self.subject = subject
+
+    def clearMessage(self):
+        self.message = ""
+
+    def addLine(self, line):
+        self.message = self.message + "\n" + line
+
+    def addString(self, string):
+        self.message = self.message + string
+
+    def send(self):
+        if self.subject and self.addrs:
+            sendEmail(" ".join(self.addrs), self.subject, self.message)
