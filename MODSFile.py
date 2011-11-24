691381e137aeffa68aaed05823d5c79af967bad4
Jason MacWilliams <jason@discoverygarden.ca>
2011-11-24 09:54:52 -0400

    Added correct etree version detection on import - kills the script if not found

--- MODSFile.py
+++ MODSFile.py
@@ -1,5 +1,33 @@
-from lxml import etree
+import sys
 import marccountries
+
+try:
+  from lxml import etree
+  print("running with lxml.etree")
+except ImportError:
+  try:
+    # Python 2.5
+    import xml.etree.cElementTree as etree
+    print("running with cElementTree on Python 2.5+")
+  except ImportError:
+    try:
+      # Python 2.5
+      import xml.etree.ElementTree as etree
+      print("running with ElementTree on Python 2.5+")
+    except ImportError:
+      try:
+        # normal cElementTree install
+        import cElementTree as etree
+        print("running with cElementTree")
+      except ImportError:
+        try:
+          # normal ElementTree install
+          import elementtree.ElementTree as etree
+          print("running with ElementTree")
+        except ImportError:
+          print("Failed to import ElementTree from any known place")
+          sys.exit(9)
+
 
 class MODSFile:
     HTML_NS = 'http://www.w2.org/TR/xhtml1/DTD/xhtml1-strict.dtd'
